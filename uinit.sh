790bb40c174080f7881b38494bd1c085c13da450
yaojie <1670015877@qq.com>
2020-04-02 21:30:04 +0800

    20200331 增加nano配置和bash配置支持

--- uinit.sh
+++ uinit.sh
@@ -4,7 +4,27 @@ SELFDIR=$(dirname "$0")
 
 cd "$SELFDIR"
 
+echo '配置vim...'
 cp -R HOME_vim ~/.vim
 cp vimrc ~/.vimrc
+
+echo '更新bash配置文件...'
+cp profile ~/.profile
+cp bashrc ~/.bashrc
+
+echo '初始化nano配置'
+cp nanorc ~/.nanorc
+
+
+DIRLIST="bin c sh tmp"
+
+cd
+
+echo '创建相关目录...'
+for d in $DIRLIST ; do
+    if [ ! d "$d" ] ; then
+        mkdir "$d"
+    fi
+done
 
 
